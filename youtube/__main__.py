3c42e545190e912aa8034c029338aeecdd8889a9
Leigh Michael Forrest <leigh@catbirdseat.io>
2024-11-28 03:39:34 -0500

    rewrite commenced

--- youtube/__main__.py
+++ youtube/__main__.py
@@ -1,24 +1,2 @@
-import argparse
-from pprint import pprint
-
-from sqlalchemy.sql.expression import select
-
-from youtube.channels import get_channel, get_channel_stats
-from youtube.db import init_db
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="A script that retrieves data from the YouTube API"
-    )
-    parser.add_argument("handles", nargs="*", help="Zero or more YouTube handles")
-
-    args = parser.parse_args()
-
-    # Setup database
-    engine, session = init_db()
-
-    handle = "@RickBeato"
-    channel = get_channel(session, handle)
-    stats = get_channel_stats(session, channel)
-
-    print(stats)
+    print("Hello World!")
--- youtube/db/engine.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from sqlalchemy import create_engine
-
-current_dir = Path(__file__).resolve().parent
-
-default_database_url = f"sqlite+pysqlite:///{current_dir}/youtube.db"
-
-engine = create_engine(default_database_url, echo=True)
